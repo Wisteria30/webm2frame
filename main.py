808b90873b5e3af3c1a318baa181cbcbbb88ff74
Wisteria30 <skymars0327@yahoo.co.jp>
2022-07-14 01:45:14 +0900

    shotが10frame以下の場合に対応

--- main.py
+++ main.py
@@ -27,22 +27,32 @@ def save_frameN(
     s   @1  @2  @3  @4  @5  @6  @7  @8  @9  @10   e
     | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 |
     """
+    delta = (
+        0
+        if (int(frame_info["endframe"]) - int(frame_info["startframe"]) - 2 * delta) < N
+        else delta
+    )
+
     with VideoFileClip(video_file_path) as clip:
         clip = clip.resize(newsize)
-        start_frame = int(frame_info["startframe"]) + delta
-        end_frame = int(frame_info["endframe"]) - delta
+        start_frame = delta
+        end_frame = int(frame_info["endframe"]) - int(frame_info["startframe"]) - delta
         cum_frame = end_frame - start_frame
         if cum_frame >= N:
             div_Nplus1 = cum_frame / (N + 1)
             frame_N = []
-            s = delta
             for i in range(N):
-                s += div_Nplus1
-                frame_N.append(s / clip.fps)
+                start_frame += div_Nplus1
+                frame_N.append(start_frame / clip.fps)
         else:
             frame_N = [i / clip.fps for i in range(start_frame, end_frame + 1)]
+
         for i, t in enumerate(frame_N):
-            clip.save_frame(op.join(save_dir, f"frame{i}.png"), t=t)
+            path = op.join(save_dir, f"frame{i:04}.png")
+            if op.exists(path):
+                print(f"{path} already exists")
+                continue
+            clip.save_frame(path, t=t)
 
 
 @click.command()
@@ -62,7 +72,7 @@ def main(video_origin, msb_dir, save_origin, delta, start, end) -> None:
     # slice
     video_dirs = video_dirs[start:end]
     msb_files = msb_files[start:end]
-    for video_dir, msb_file in tqdm(zip(video_dirs, msb_files)):
+    for video_dir, msb_file in tqdm(zip(video_dirs, msb_files), total=len(video_dirs)):
         msb = read_tsv(op.join(msb_dir, msb_file))
         video_paths = natsorted(
             list(
@@ -72,11 +82,12 @@ def main(video_origin, msb_dir, save_origin, delta, start, end) -> None:
                 )
             )
         )
-        for i, video_path in enumerate(video_paths):
+        for i, video_path in tqdm(enumerate(video_paths), total=len(video_paths)):
             save_dir = op.join(save_origin, video_dir, video_path)
             os.makedirs(save_dir, exist_ok=True)
             video_file_path = op.join(video_origin, video_dir, video_path)
             save_frameN(video_file_path, save_dir, msb[i], delta)
+    print("done!")
 
 
 if __name__ == "__main__":
